f2147ea0d2bb280c446c12b8f9fbf4b8197baca4
Elton <eltonp3103@gmail.com>
2019-11-26 23:57:09 -0500

    simple installation file setup done

--- install.sh
+++ install.sh
@@ -3,17 +3,24 @@
 BASE_DIR=$HOME
 
 # installing brew
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # git
-# brew install git
+brew install git
 
 # vim
-# brew install vim
+brew install vim
 
 # fish
-# brew install fish
+brew install fish
 
-# links
-ln -s ./.vimrc "${BASE_DIR}/.vimrc"
+# symlinks
+ln -s ./vim/.vimrc "${BASE_DIR}/.vimrc"
+ln -s ./tmux/.tmux.conf "${BASE_DIR}/.tmux.conf"
+mkdir "${BASE_DIR}/.config"
+ln -s ./fish/.config/omf "${BASE_DIR}/.config/omf" 
+
+# oh-my-fish
+curl -L https://get.oh-my.fish > install
+fish install --path=~/.local/share/omf --config=~/.config/omf
 
