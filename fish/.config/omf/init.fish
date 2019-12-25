0ff78538d87d0066a825c160863c88f554e3cef8
Elton <eltonp3103@gmail.com>
2019-12-24 22:57:09 -0500

    used gnu stow to set up symlinks

---
+++ fish/.config/omf/init.fish
@@ -0,0 +1,10 @@
+echo "Welcome back 1ntEgr8"
+
+alias yalla="exit"
+alias hackgt="cd ~/Documents/Projects/hackgt"
+alias chrome="open -a 'Google Chrome'"
+alias hn="~/Documents/Projects/hn/target/release/hn"
+
+source $HOME/.cargo/env
+export GOPATH=$HOME/Documents/Projects/bogo
+
