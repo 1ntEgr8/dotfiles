615555dc3aa33faf8bb727a74c42f4091f16fa1e
Elton <eltonp3103@gmail.com>
2020-08-05 22:32:02 -0400

    update config.fish with gcloud sdk config; update tmux config post upgrade

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,5 +1,5 @@
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/eltonpinto/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/eltonpinto/Downloads/google-cloud-sdk/path.fish.inc'; end
-
 starship init fish | source
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/eltonpinto/google-cloud-sdk/path.fish.inc' ]; . '/Users/eltonpinto/google-cloud-sdk/path.fish.inc'; end
