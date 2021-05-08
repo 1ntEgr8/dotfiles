5b15262abd5c08a97bf383a462c5faa392fb96a7
Elton <eltonp3103@gmail.com>
2021-05-07 23:23:57 -0400

    update fish shell config

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,5 +1,3 @@
-starship init fish | source
-
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/eltonpinto/google-cloud-sdk/path.fish.inc' ]; . '/Users/eltonpinto/google-cloud-sdk/path.fish.inc'; end
 
