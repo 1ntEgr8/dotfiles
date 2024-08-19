7ee834fd61c30991d6d5b6bdb0e4f6e9c18c6a07
Elton Leander Pinto <eltonp3103@gmail.com>
2024-08-19 17:28:31 -0400

    Fix clipboard and color issues

--- editor/nvim/lua/config/init.lua
+++ editor/nvim/lua/config/init.lua
@@ -1,1 +1,17 @@
 require("config.lazy")
+
+-- Alacritty doesn't support the XTGETTCAP sequence. Manually turn on termguicolors.
+vim.o.termguicolors = true
+
+ -- clipboard setup
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
